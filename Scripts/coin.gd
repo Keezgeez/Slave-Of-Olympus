bec0c60b4b684e1005212528dc705faa5cb9267b
j-l1 <135350971+j-l1@users.noreply.github.com>
2024-06-11 14:10:50 +0800

    coin fade

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,11 +1,23 @@
 extends Area2D
 
-var collected = false
+#var collected = false
 
 func _on_body_entered(body):
-	if not collected:
-		$AudioStreamPlayer2D.play()
-		print("+1 coin")
-		hide()
-		collected = true
+	#if not collected:
+	#	$AudioStreamPlayer2D.play()
+	#	print("+1 coin")
+	#	hide()
+	#	collected = true
 		# Disable collision
+		
+		if body.name == "Slave":
+			var tween = create_tween()
+			
+			tween.tween_property(self, "position", position + Vector2(0, -30), 0.5)
+			
+			tween.tween_property(self, "modulate:a", 0.0, 0.5)
+			
+			$AudioStreamPlayer2D.play()
+			print("+1 coin")
+			
+			tween.tween_callback(self.queue_free)
