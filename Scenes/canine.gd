f7a42f8a60f5332c7466702e830087e8a993cb93
Keezgeez <135505653+Keezgeez@users.noreply.github.com>
2024-06-17 20:32:15 +0800

    UpdateNiPepe17

--- Scenes/canine.gd
+++ Scenes/canine.gd
@@ -17,5 +17,11 @@ func _process(delta):
 	if ray_cast_right.is_colliding():
 		direction = -1
 		canine_enemy.flip_h = true
+		
+
 	
 	position.x += direction * SPEED * delta
+
+
+func _on_killzone_body_entered(body):
+	pass # Replace with function body.
