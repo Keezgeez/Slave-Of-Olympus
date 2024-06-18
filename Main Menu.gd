42c5b5b4313308a429076a1ab2fbe71b2e3c3eef
Keezgeez <135505653+Keezgeez@users.noreply.github.com>
2024-06-18 21:59:36 +0800

    UpdateMenu

--- Main Menu.gd
+++ Main Menu.gd
@@ -1,10 +1,15 @@
 extends Control
 
+
+
+
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
 
+
 func _on_how_to_play_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main_menuOptions.tscn")
+
 
 func _on_quit_pressed():
 	get_tree().quit()
