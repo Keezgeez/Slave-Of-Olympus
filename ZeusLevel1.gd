f527e639c56adf27e2d7292a1711fbd95e88d361
j-l1 <135350971+j-l1@users.noreply.github.com>
2024-07-03 03:40:44 +0800

    zeus of level 1 2 and 3

---
+++ ZeusLevel1.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+var entered = false
+
+func _on_body_entered(body):
+	entered = true
+	print ("detected")
+
+
+func _on_body_exited(body):
+	entered = false
+	print ("notsodetected")
+
+func _process(delta):
+	if entered == true:
+		if Input.is_action_just_pressed("ui_select"):
+			get_tree().change_scene_to_file("res://Scenes/Level_2.tscn")
